f54b308f3555035626a5d5a99d0fe4d462f2260b
NaftaliRisemberg <n9742686@gmail.com>
2024-09-05 12:51:31 +0300

    Add project files.

---
+++ SyberMangmentTestWeek1/DefenceStrategiesBST.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyberMangmentTestWeek1
+{
+	public class DefenceStrategiesBST
+	{
+		public int MinSeverity {  get; set; }
+		public int MaxSeverity { get; set; }
+		public DefenceStrategiesBST? Left {  get; set; }
+		public DefenceStrategiesBST? Right { get;set; }
+		public List<string> Defenses { get; set; }
+
+		public DefenceStrategiesBST(int minSeverity, int maxSeverity, List<string> defenses)
+		{
+			MinSeverity = minSeverity;
+			MaxSeverity = maxSeverity;
+			Left = null;
+			Right = null;
+			Defenses = defenses;
+		}
+	}
+}
